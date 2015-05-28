3230729f4fdd874e9817e9add0f5cd507a98c608
DAM1 <DAM1@PC705-PC>
2015-05-28 09:55:54 +0200

    ordenacion alfa

--- src/ESTRUCTURAS_garaje.c
+++ src/ESTRUCTURAS_garaje.c
@@ -1,25 +1,40 @@
 /*
  ============================================================================
  Name        : ESTRUCTURAS_garaje.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Author      : Serulvan
+ Version     : 0.0
+ Copyright   : psa
+ Description : trabajo con estructuras
  ============================================================================
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int size(struct Coche taller[]){
-	return sizeof(taller)/sizeof(taller[0]);
+int size(struct Coche taller[]) {
+	return sizeof(taller) / sizeof(taller[0]);
 }
 
-int buscar (struct Coche taller[], char matricula[]){
+int buscar(struct Coche taller[], char matricula[]) {
 	int i = 0;
 	do {
-		if(strcasecmp(matricula, taller[i].matricula)==0)return i;
-	} while (++i<size(taller));
+		if (strcasecmp(matricula, taller[i].matricula) == 0);
+			return i;
+	} while (++i < size(taller));
+}
+
+void ordenar(struct Coche taller[]) {
+	struct Coche aux;
+	int i,j;
+	for (i = 0; i < size(taller)-1; ++i) {
+		for (j = 1; j < size(taller); ++j) {
+			if (strcasecmp(taller[i].matricula, taller[j].matricula) == 1) {
+				aux = taller[i];
+				taller[i] = taller[j];
+				taller[j] = aux;
+			}
+		}
+	}
 }
 
 int main(void) {
