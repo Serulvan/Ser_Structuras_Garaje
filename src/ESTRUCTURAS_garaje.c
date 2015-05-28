bf63ec8c2d6757467386cb99fd55b93c38013609
DAM1 <DAM1@PC705-PC>
2015-05-28 09:18:14 +0200

    busqueda y tamaño

--- src/ESTRUCTURAS_garaje.c
+++ src/ESTRUCTURAS_garaje.c
@@ -11,6 +11,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int size(struct Coche taller[]){
+	return sizeof(taller)/sizeof(taller[0]);
+}
+
+int buscar (struct Coche taller[], char matricula[]){
+	int i = 0;
+	do {
+		if(strcasecmp(matricula, taller[i].matricula)==0)return i;
+	} while (++i<size(taller));
+}
+
 int main(void) {
 	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
