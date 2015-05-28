47c3d5a66a42be0798fa98c0b3608e61c307f8a6
DAM1 <DAM1@PC705-PC>
2015-05-28 11:29:59 +0200

    insertar añadido

--- src/ESTRUCTURAS_garaje.c
+++ src/ESTRUCTURAS_garaje.c
@@ -18,15 +18,16 @@ int size(struct Coche taller[]) {
 int buscar(struct Coche taller[], char matricula[]) {
 	int i = 0;
 	do {
-		if (strcasecmp(matricula, taller[i].matricula) == 0);
+		if (strcasecmp(matricula, taller[i].matricula) == 0)
 			return i;
 	} while (++i < size(taller));
+	return -1;
 }
 
 void ordenar(struct Coche taller[]) {
 	struct Coche aux;
-	int i,j;
-	for (i = 0; i < size(taller)-1; ++i) {
+	int i, j;
+	for (i = 0; i < size(taller) - 1; ++i) {
 		for (j = 1; j < size(taller); ++j) {
 			if (strcasecmp(taller[i].matricula, taller[j].matricula) == 1) {
 				aux = taller[i];
@@ -37,7 +38,20 @@ void ordenar(struct Coche taller[]) {
 	}
 }
 
+int insertar(struct Coche coche, struct Coche taller[]) {
+	if (buscar(taller, coche.matricula) != -1) {
+		return 2;
+	} else if (buscar(taller, NULL) != -1) {
+		taller[bucar(taller, NULL)] = coche;
+		return 0;
+	} else
+		return 1;
+}
+
+
+
 int main(void) {
+	setbuf(stdout, NULL);
 	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
